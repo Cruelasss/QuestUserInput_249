e0a05b1671aa435c954f2888385f523796d42e28
Cruelasss <irula5198@gmail.com>
2025-10-30 08:53:09 +0700

    variable untuk menyimpan data yang diperoleh dari komponer

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -9,3 +9,8 @@ fun FormatDataDiri(modifier: Modifier
     var textAlamat by remember { mutableStateOf(value ="") }
     var textJK by remember { mutableStateOf(value ="") }
 
+    //variabel untuk menyimpan data yang diperoleh dari komponer
+    var nama by remember { mutableStateOf(value ="") }
+    var alamat by remember { mutableStateOf(value ="") }
+    var jenis by remember { mutableStateOf(value ="") }
+
