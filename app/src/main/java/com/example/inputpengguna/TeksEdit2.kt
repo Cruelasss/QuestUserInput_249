ca36821b6240d7200c246f72178ed40f89f37e14
Cruelasss <irula5198@gmail.com>
2025-11-01 14:29:48 +0700

    Tambah state untuk field form

--- app/src/main/java/com/example/inputpengguna/TeksEdit2.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit2.kt
@@ -20,3 +20,9 @@ import com.example.inputpengguna.ui.theme.Purple40
 
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+
