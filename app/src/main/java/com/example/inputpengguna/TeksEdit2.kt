88fb134c51ce29342eeeac9425fa73fad2e473a0
Cruelasss <irula5198@gmail.com>
2025-11-01 14:25:07 +0700

    Membuat Dasar Code

--- app/src/main/java/com/example/inputpengguna/TeksEdit2.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit2.kt
@@ -1,2 +1,22 @@
 package com.example.inputpengguna
 
+import androidx.compose.foundation.background
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.Divider
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import com.example.inputpengguna.ui.theme.Purple40
+
+
+
+@Composable
+fun FormDataDiri(modifier: Modifier = Modifier) {
